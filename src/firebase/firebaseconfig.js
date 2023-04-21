a5bcf86d3449f73f5bb04811e9d5b6b4b60b2b68
Elizabeth4812 <elizabethjerezr@gmail.com>
2023-04-20 20:33:02 -0400

    se modifican datos

--- src/firebase/firebaseconfig.js
+++ src/firebase/firebaseconfig.js
@@ -1,8 +1,19 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-// TODO: Replace the following with your app's Firebase project configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  //...
+  apiKey: "AIzaSyBDrPv7PzEiVw3t9vI81BGUgP1lFUBTIJM",
+  authDomain: "devsafio-us.firebaseapp.com",
+  projectId: "devsafio-us",
+  storageBucket: "devsafio-us.appspot.com",
+  messagingSenderId: "377347257592",
+  appId: "1:377347257592:web:45a81af40226f97f58b70d",
+  measurementId: "G-1VR36EJYBJ"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
